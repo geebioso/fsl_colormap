7af460ac36c6567293d58f10e1b22d5b42d2a02d
Garren Gaut <gbius06@gmail.com>
2018-02-20 13:29:10 -0600

    changed arguments to varagin to allow default number of colors

--- fsl_colormap.m
+++ fsl_colormap.m
@@ -1,10 +1,10 @@
-function map = fsl_colormap(n)
+function map = fsl_colormap(varargin)
 
-% This function computes the matlab colors for the FSLview map 
+% This function computes the matlab colors for the FSLview map
 % defined in the Human Connectome Project Workbench Viewer
 
 % Scalar values for each color are taken from the Workbench source code
-% at https://github.com/Washington-University/workbench/blob/master/src/Files/PaletteFile.cxx: 
+% at https://github.com/Washington-University/workbench/blob/master/src/Files/PaletteFile.cxx:
 
 % palFSLView.addScalarAndColor( 1.0f, '_red_yellow_interp_yellow');
 % palFSLView.addScalarAndColor( 0.00001f, '_red_yellow_interp_red');
@@ -20,15 +20,23 @@ function map = fsl_colormap(n)
 % this->addColor('_blue_lightblue_interp_lightblue',  0, 255, 255 );
 % this->addColor('_fslview_zero', 0, 0, 0);
 
-%% Define color values and interpolate 
-scalars = [ 0.00001, 1.0, -0.00001, -1.0, 0.0000099, -0.0000099]'; 
+if nargin < 1
+    n = size(get(gcf, 'Colormap'), 1);
+elseif nargin == 1
+    n = varargin{1};
+else
+    error('function takes 0 or 1 input');
+end
 
-R = [ 255, 255, 0, 0, 0, 0 ]'; 
-G = [ 0,   255, 0, 255, 0, 0]'; 
-B = [ 0, 0, 255, 255, 0, 0]'; 
+%% Define color values and interpolate
+scalars = [ 0.00001, 1.0, -0.00001, -1.0, 0.0000099, -0.0000099]';
+
+R = [ 255, 255, 0, 0, 0, 0 ]';
+G = [ 0,   255, 0, 255, 0, 0]';
+B = [ 0, 0, 255, 255, 0, 0]';
 
 [sort_scalars, ii] = sort(scalars);
-values = [R, G, B]/255; 
-values = values(ii,:); 
+values = [R, G, B]/255;
+values = values(ii,:);
 
 map = interp1(sort_scalars, values, linspace(-1,1,n), 'linear');
